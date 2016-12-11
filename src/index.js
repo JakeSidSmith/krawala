68f296e1e0caeba8370bbb850011e4a5af785208
Jake 'Sid' Smith <jake@dabapps.com>
2016-12-11 19:45:43 +0000

    Abstract getData function

--- src/index.js
+++ src/index.js
@@ -13,6 +13,66 @@
   var MATCHES_NO_PROTOCOL = /^\/\//;
   var MATCHES_RELATIVE_URL = /\.?\//;
 
+  function getData (html, baseDomain) {
+    var $ = cheerio.load(html);
+
+    var links = _.chain($('a[href]').toArray().map(function (el) {
+      var element = $(el);
+
+      return element.attr('href');
+    }))
+    .groupBy(_.identity)
+    .map(function (values, key) {
+      return {
+        url: key,
+        references: values.length
+      };
+    })
+    .sortBy('url')
+    .groupBy(function (link) {
+      if (link.url.indexOf('#') === 0) {
+        return 'same-page';
+      }
+
+      if (link.url.indexOf('tel:') === 0) {
+        return 'phone';
+      }
+
+      if (link.url.indexOf('mailto:') === 0) {
+        return 'email';
+      }
+
+      var linkDomain = parseDomain(link.url.replace(MATCHES_NO_PROTOCOL, ''));
+
+      if (!linkDomain) {
+        var relativeUrl = MATCHES_RELATIVE_URL.exec(link.url);
+        return relativeUrl && relativeUrl.index === 0 ? 'internal' : 'unknown';
+      }
+
+      return linkDomain.domain === baseDomain.domain &&
+        linkDomain.tld === baseDomain.tld ? 'internal' : 'external';
+    })
+    .value();
+
+    return {
+      title: $('title').text() || null,
+      charset: $('meta[charset]').attr('charset') || null,
+      meta: _.object($('meta[name],meta[property]').toArray().map(function (el) {
+        var element = $(el);
+
+        return [element.attr('name') || element.attr('property'), element.attr('content')];
+      })),
+      links: {
+        internal: links.internal || [],
+        external: links.external || [],
+        'same-page': links['same-page'] || [],
+        email: links.email || [],
+        phone: links.phone || [],
+        unknown: links.unknown || []
+      }
+    };
+  }
+
   function crawl(options, callback) {
     if (!options.url) {
       error('No url specified');
@@ -38,64 +98,7 @@
         if (err) {
           error(err);
         } else {
-          var html = res.text;
-          var $ = cheerio.load(html);
-
-          var links = _.chain($('a[href]').toArray().map(function (el) {
-            var element = $(el);
-
-            return element.attr('href');
-          }))
-          .groupBy(_.identity)
-          .map(function (values, key) {
-            return {
-              url: key,
-              references: values.length
-            };
-          })
-          .sortBy('url')
-          .groupBy(function (link) {
-            if (link.url.indexOf('#') === 0) {
-              return 'same-page';
-            }
-
-            if (link.url.indexOf('tel:') === 0) {
-              return 'phone';
-            }
-
-            if (link.url.indexOf('mailto:') === 0) {
-              return 'email';
-            }
-
-            var linkDomain = parseDomain(link.url.replace(MATCHES_NO_PROTOCOL, ''));
-
-            if (!linkDomain) {
-              var relativeUrl = MATCHES_RELATIVE_URL.exec(link.url);
-              return relativeUrl && relativeUrl.index === 0 ? 'internal' : 'unknown';
-            }
-
-            return linkDomain.domain === baseDomain.domain &&
-              linkDomain.tld === baseDomain.tld ? 'internal' : 'external';
-          })
-          .value();
-
-          json[url] = {
-            title: $('title').text() || null,
-            charset: $('meta[charset]').attr('charset') || null,
-            meta: _.object($('meta[name],meta[property]').toArray().map(function (el) {
-              var element = $(el);
-
-              return [element.attr('name') || element.attr('property'), element.attr('content')];
-            })),
-            links: {
-              internal: links.internal || [],
-              external: links.external || [],
-              'same-page': links['same-page'] || [],
-              email: links.email || [],
-              phone: links.phone || [],
-              unknown: links.unknown || []
-            }
-          };
+          json[url] = getData(res.text, baseDomain);
 
           if (urls.length) {
 
