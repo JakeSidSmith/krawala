07dcab9ebc08c5138c072e891e9c73c0ad133df0
Jake 'Sid' Smith <jake@dabapps.com>
2016-12-11 17:42:46 +0000

    Move crawl options to crawl command

--- src/cli.js
+++ src/cli.js
@@ -12,37 +12,27 @@
   .command('crawl', 'Crawl a domain')
   .example('$0 crawl domain.com --depth 100', '(Crawl a domain to a depth of 100)')
   .alias('crawl', 'c')
-  .option(
-    'url',
-    {
-      group: 'crawl options:',
+  .command('crawl', 'Crawl a domain', {
+    url: {
       alias : 'u',
       required: true,
       type: 'string',
       describe: 'URL to crawl'
-    }
-  )
-  .option(
-    'depth',
-    {
-      group: 'crawl options:',
+    },
+    depth: {
       alias : 'd',
       default : 10,
       type: 'number',
       describe: 'Depth to crawl'
-    }
-  )
-  .option(
-    'format',
-    {
-      group: 'crawl options:',
+    },
+    format: {
       alias : 'f',
       default : 'json',
       type: 'string',
       describe: 'Format to return',
       choices: ['json']
     }
-  )
+  })
   .help('help')
   .alias('help', 'h')
   .version('version', 'Return the version number', version)
