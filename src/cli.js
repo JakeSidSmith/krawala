d7579b0e4f61c77153761f0526fc657a944c6667
Jake 'Sid' Smith <jake@dabapps.com>
2016-12-11 17:43:55 +0000

    Group crawl options

--- src/cli.js
+++ src/cli.js
@@ -7,24 +7,29 @@
   var packageJson = require('../package.json');
   var version = packageJson.version;
 
+  var CRAWL_OPTIONS = 'Crawl options:';
+
   var argv = yargs
   .usage('Usage: $0 <command> [options]')
   .example('$0 crawl -u domain.com -d 100', '(Crawl a URL to a depth of 100)')
   .alias('crawl', 'c')
   .command('crawl', 'Crawl a domain', {
     url: {
+      group: CRAWL_OPTIONS,
       alias : 'u',
       required: true,
       type: 'string',
       describe: 'URL to crawl'
     },
     depth: {
+      group: CRAWL_OPTIONS,
       alias : 'd',
       default : 10,
       type: 'number',
       describe: 'Depth to crawl'
     },
     format: {
+      group: CRAWL_OPTIONS,
       alias : 'f',
       default : 'json',
       type: 'string',
