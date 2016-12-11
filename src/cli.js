c5b64dcd84e6dfc3f77f63f08f160e445af018e7
Jake 'Sid' Smith <jake@dabapps.com>
2016-12-11 15:25:24 +0000

    Setup yargs

--- src/cli.js
+++ src/cli.js
@@ -1,1 +1,32 @@
 #! /usr/bin/env node
+'use strict';
+
+(function () {
+
+  var yargs = require('yargs');
+  var packageJson = require('../package.json');
+  var version = packageJson.version;
+
+  var argv = yargs
+  .usage('Usage: $0 <command> [options]')
+  .command('crawl', 'Crawl a domain')
+  .example('$0 crawl domain.com --depth 100', 'Crawl a domain to a depth of 100')
+  .alias('crawl', 'c')
+  .option(
+    'depth',
+    {
+      alias : 'd',
+      default : 10,
+      type: 'number',
+      describe: 'Depth to crawl'
+    }
+  )
+  .help('help')
+  .alias('help', 'h')
+  .version('version', 'Return the version number', version)
+  .alias('version', 'v')
+  .argv;
+
+  console.log(argv);
+
+})();
