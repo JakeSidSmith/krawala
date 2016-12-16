bb6be554173a95b2060e1e63db78dc34e59725ca
Jake 'Sid' Smith <jake@dabapps.com>
2016-12-16 13:59:29 +0000

    Add protocol to CLI example

--- src/cli.js
+++ src/cli.js
@@ -17,7 +17,7 @@
   .require(1, 1)
   .strict()
   .usage('Usage: $0 <command> [options]')
-  .example('$0 crawl -u domain.com -d 100', '(Crawl a URL to a depth of 100)')
+  .example('$0 crawl -u http://domain.com -d 100', '(Crawl a URL to a depth of 100)')
   .alias('crawl', 'c')
   .command('crawl', 'Crawl a domain', {
     url: {
