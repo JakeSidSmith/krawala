01f40f4143198dd1a2cd8c8c1bc9b9117505b4fa
Jake 'Sid' Smith <jake@dabapps.com>
2017-10-16 20:07:43 +0100

    Better error handling

--- ts/utils.ts
+++ ts/utils.ts
@@ -3,7 +3,7 @@ import * as parseDomain from 'parse-domain';
 import * as parseUrl from 'url-parse';
 
 export const error = (message: string) => {
-  if (typeof process === 'object') {
+  if (typeof process === 'object' && typeof process.exit === 'function') {
     console.error(message); // tslint:disable-line no-console
     process.exit(1);
   } else {
@@ -33,24 +33,21 @@ export const resolveUrl = (url: string, parentUrl: string) => {
 
 export const validateBaseUrl = (url?: string): true | void => {
   if (typeof url !== 'string' || !url) {
-    error('Invalid URL. URL cannot be empty');
-    return;
+    return error('Invalid URL. URL cannot be empty');
   }
 
   const urlInfo = parseUrl(url);
   const domainInfo = parseDomain(url);
 
   if (!urlInfo.protocol) {
-    error('Invalid URL. URL must have an explicit protocol e.g. \'http://\'');
-    return;
+    return error('Invalid URL. URL must have an explicit protocol e.g. \'http://\'');
   }
 
   if (!domainInfo || !domainInfo.domain || !domainInfo.tld) {
-    error(
+    return error(
       'Invalid URL. Could not resolve a domain. ' +
       'Ensure the URL has a valid protocol, domain, & TLD e.g. \'http://domain.com\''
     );
-    return;
   }
 
   return true;
