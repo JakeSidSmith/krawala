348a4e9a440e45b44d82155809b37e6c2c3fb008
Jake 'Sid' Smith <jake@dabapps.com>
2017-10-16 18:57:39 +0100

    Improve jargs types

--- types/jargs/index.d.ts
+++ types/jargs/index.d.ts
@@ -1,6 +1,8 @@
 declare module 'jargs' {
 
-  export type collect = (rootNode: Help | Program) => Tree;
+  export type HelpOrProgram = Help | Program;
+
+  export function collect (rootNode: HelpOrProgram): Tree;
 
   export interface ArgsOrKWArgs {
     [index: string]: string | undefined;
@@ -12,7 +14,7 @@ declare module 'jargs' {
 
   export interface Tree {
     name: string;
-    command: Tree;
+    command: Tree |  null;
     kwargs: ArgsOrKWArgs;
     flags: Flags;
     args: ArgsOrKWArgs;
@@ -27,7 +29,7 @@ declare module 'jargs' {
     description?: string;
     usage?: string;
     examples?: string[];
-    callback: (tree: Tree) => any;
+    callback?: (tree: Tree, parentTree?: Tree, returned?: any) => any;
   }
 
   export interface CommandProps {
@@ -35,7 +37,7 @@ declare module 'jargs' {
     alias?: string;
     usage?: string;
     examples?: string[];
-    callback: (tree: Tree) => any;
+    callback?: (tree: Tree, parentTree?: Tree, returned?: any) => any;
   }
 
   export interface KWArgProps {
@@ -56,17 +58,86 @@ declare module 'jargs' {
     type?: string;
   }
 
-  export type RequiredChild = Command | KWArg | Flag | Arg;
-  export type ProgramOrCommandChild = Command | KWArg | Flag | Arg | Required | RequireAll | RequireAny;
+  export interface Help {
+    _type: 'help';
+    name: string;
+    options: HelpProps;
+  }
 
-  export type Help = (name: string, props: HelpProps, program: Program) => Tree;
-  export type Program = (name: string, props: ProgramProps, ...nodes: ProgramOrCommandChild[]) => Tree;
-  export type Command = (name: string, props: CommandProps, ...nodes: ProgramOrCommandChild[]) => Tree;
-  export type KWArg = (name: string, props: KWArgProps) => Tree;
-  export type Flag = (name: string, props: FlagProps) => Tree;
-  export type Arg = (name: string, props: ArgProps) => Tree;
-  export type Required = (node: RequiredChild) => Tree;
-  export type RequireAll = (...nodes: RequiredChild[]) => Tree;
-  export type RequireAny = (...nodes: RequiredChild[]) => Tree;
+  export interface Program {
+    _type: 'program';
+    name: string;
+    options: ProgramProps;
+    _requireAll: ProgramOrCommandChild[];
+    _requireAny: ProgramOrCommandChild[];
+    children: ProgramOrCommandChild[];
+  }
+
+  export interface Command {
+    _type: 'command';
+    name: string;
+    options: CommandProps;
+    _requireAll: ProgramOrCommandChild[];
+    _requireAny: ProgramOrCommandChild[];
+    children: ProgramOrCommandChild[];
+  }
+
+  export interface KWArg {
+    _type: 'kwarg';
+    name: string;
+    options: KWArgProps;
+  }
+
+  export interface Flag {
+    _type: 'flag';
+    name: string;
+    options: FlagProps;
+  }
+
+  export interface Arg {
+    _type: 'arg';
+    name: string;
+    options: ArgProps;
+  }
+
+  export interface Required {
+    _type: 'required';
+    children: RequiredChild[];
+  }
+
+  export interface RequireAll {
+    _type: 'require-all';
+    children: RequiredChild[];
+  }
+
+  export interface RequireAny {
+    _type: 'require-any';
+    children: RequiredChild[];
+  }
+
+  export type RequiredChild =
+    Command |
+    KWArg |
+    Flag |
+    Arg;
+
+  export type ProgramOrCommandChild =
+    Command |
+    KWArg |
+    Flag |
+    Arg |
+    Required |
+    RequireAll |
+    RequireAny;
+
+  export function Help (name: string, props: HelpProps | null | undefined, program: Program): Help;
+  export function Program (name: string, props?: ProgramProps | null, ...nodes: ProgramOrCommandChild[]): Program;
+  export function Command (name: string, props?: CommandProps | null, ...nodes: ProgramOrCommandChild[]): Command;
+  export function KWArg (name: string, props?: KWArgProps | null): KWArg;
+  export function Flag (name: string, props?: FlagProps | null): Flag;
+  export function Arg (name: string, props?: ArgProps | null): Arg;
+  export function Required (node: RequiredChild): Required;
+  export function RequireAll (...nodes: RequiredChild[]): RequireAll;
+  export function RequireAny (...nodes: RequiredChild[]): RequireAny;
 
 }
