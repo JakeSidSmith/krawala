d3d5dc1f508a44c839d37f19636652477a66ae76
Jake 'Sid' Smith <jake@dabapps.com>
2017-10-16 21:58:08 +0100

    Types for results

--- src/types.ts
+++ src/types.ts
@@ -6,3 +6,50 @@ export interface Progress {
   failed: number;
   progressMade: boolean;
 }
+
+export interface Crawlable {
+  url: string;
+  resolved: string;
+  failed: boolean;
+  status: number;
+  type: string | null;
+}
+
+export type Content = Partial<{
+  h1: string;
+  h2: string;
+  h3: string;
+  p: string;
+}>
+
+export type Meta = Partial<{
+  name: string;
+  content: string;
+}>
+
+export type Link = Partial<{
+  rel: string;
+  type: string;
+  href: string;
+}>
+
+export type Script = Partial<{
+  type: string;
+  src: string;
+}>
+
+export type Image = Partial<{
+  src: string;
+}>
+
+export type Result = Partial<{
+  charset: string;
+  title: string;
+  wordCount: number;
+  content: Content;
+  meta: Meta[];
+  links: Link[];
+  scripts: Script[];
+  images: Image[];
+  hrefs: Crawlable[];
+}> & Crawlable
